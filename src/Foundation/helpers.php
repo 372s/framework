3cfb4bb9fc4ce6d6e0c961489d3ab6827120b9f9
wq455133 <wq455133@163.com>
2018-11-08 23:45:24 +0800

    update helpers.php

--- src/Foundation/helpers.php
+++ src/Foundation/helpers.php
@@ -1,14 +1,23 @@
 <?php
 
 
-if (! function_exists('windows_os')) {
-    /**
-     * Determine whether the current environment is Windows based.
-     *
-     * @return bool
-     */
-    function windows_os()
-    {
-        return strtolower(substr(PHP_OS, 0, 3)) === 'win';
-    }
+/**
+ * Determine whether the current environment is Windows based.
+ *
+ * @return bool
+ */
+function windows_os()
+{
+    return strtolower(substr(PHP_OS, 0, 3)) === 'win';
+}
+
+/**
+ * 导入视图
+ * @throws \InvalidArgumentException
+ * @param mixed $name
+ * @return object
+ */
+function view($name = null)
+{
+    return View::make($name);
 }
